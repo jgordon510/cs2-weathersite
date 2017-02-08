705d98fd89b55cfeab1b0265022bc6c7582f1c38
jgordon510 <jgordon.oakland@gmail.com>
2017-02-08 15:21:28 +0000

    After first day

---
+++ script.js
@@ -0,0 +1,3 @@
+bootbox.prompt("<p>We do, doodley do, doodley do, doodely do, <p>What we must, muddily must, muddily must, muddily must;<p>Muddily do, muddily do, muddily do, muddily do,<p>Until we bust, bodily bust, bodily bust, bodily bust.", function (result) {
+    bootbox.alert(result);
+})
